e4d45cc87fe193e207ba51dd6a1b38e49e2aa548
liuziyang <1872405512@qq.com>
2020-08-30 16:55:53 +0800

    ready to first push

--- P1003_blanket.cpp
+++ P1003_blanket.cpp
@@ -11,9 +11,18 @@ int main()
     for(int i=0; i<numBlankets; i++){
         int x, y, xLen, yLen;
         cin>>x>>y>>xLen>>yLen;
+        vector<int> tmpItem = {x, y, xLen, yLen};
+        blankets.push_back(tmpItem);
     }
     int dotX, dotY;
     cin>>dotX>>dotY;
     cout<<numBlankets<<dotX<<dotY<<endl;
+    int ans = -1;
+    for(int i=0; i<numBlankets; i++){
+        if(blankets[i][0]<=dotX && blankets[i][0]+blankets[i][2]>=dotX && blankets[i][1]<=dotY && blankets[i][1]+blankets[i][3]>=dotY){
+            ans = i+1;
+        }
+    }
+    cout<<ans;
     return 0;
 }
